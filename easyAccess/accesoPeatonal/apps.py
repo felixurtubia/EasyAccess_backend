8706813a6e8ccdf07da0968c42686b2b9f0c79f4
felixurtubia <felix.urtubia@gmail.com>
2018-06-13 16:11:47 -0400

    Cambio de metodología para hacer el projecto
    Ahora se usa django + python para reconococimiento facial y mongodb + nodejs para la lógica y datos de usuarios.

--- easyAccess/accesoPeatonal/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AccesopeatonalConfig(AppConfig):
-    name = 'accesoPeatonal'
--- easyAccess/controlInvitados/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ControlinvitadosConfig(AppConfig):
-    name = 'controlInvitados'
