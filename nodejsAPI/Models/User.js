635ec46ac9c14a95b1675d7dd6175433c698e2dd
allanviveros <allan.viveros.14@sansano.usm.cl>
2018-06-22 23:42:05 -0400

    se actualizo el modelo de los usuarios

--- nodejsAPI/Models/User.js
+++ nodejsAPI/Models/User.js
@@ -1,10 +1,15 @@
 'use strict'
 
 const mongoose = require('mongoose');
-const schema = mongoose.Schema
+const schema = mongoose.Schema;
 
 const userSchema = schema({
+    _id : mongoose.Schema.Types.ObjectId,
     nombre : String,
     apellido : String,
+    rut : String,
+    edad: String,
     fotos : [{type: String}] //arreglo de fotos
-})
+});
+
+module.exports = mongoose.model('User', userSchema);
