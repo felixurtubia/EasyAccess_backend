9bc56d32271d761a5a2834f74f77d93b31d69f54
felixurtubia <felix.urtubia@gmail.com>
2018-07-20 19:18:33 -0400

    Integración y se reciben imagenes en Base64
    Actualización para docker

--- recognitionAPI/face_rest/serializers.py
+++ recognitionAPI/face_rest/serializers.py
@@ -3,11 +3,13 @@ from face.models import Person
 from drf_extra_fields.fields import Base64ImageField
 
 class PersonSerializer(serializers.ModelSerializer):
-    #image1 = Base64ImageField()
-    image1 = serializers.ImageField(required=False)
-    image2 = serializers.ImageField(required=False)
-    image3 = serializers.ImageField(required=False)
-    id_mongo = serializers.IntegerField(required=False)
+    image1 = Base64ImageField()
+    image2 = Base64ImageField()
+    image3 = Base64ImageField()
+    #image1 = serializers.ImageField(required=False)
+    #image2 = serializers.ImageField(required=False)
+    #image3 = serializers.ImageField(required=False)
+    id_mongo = serializers.CharField(required=False)
     class Meta:
         model = Person
         fields = ['pk','created','updated','id_mongo', 'image1', 'image2', 'image3']
