8c51a27d267aee948b2f459e2bee7e359a612c5d
Benjamin Miranda <benjamin.miranda.12@sansano.usm.cl>
2018-07-23 01:23:44 -0400

    fix route get to post

--- nodejsAPI/Routes/User.js
+++ nodejsAPI/Routes/User.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 //RUTAS
 router.get('/', userCtrl.getUser); // get all users
-router.get('/getId', userCtrl.postIdentification);
+router.post('/getId', userCtrl.postIdentification);
 router.get('/:userRut', userCtrl.getUserRut); // get user by rut
 router.post('/', userCtrl.postUser); // create a new user
 router.delete('/:userRut', userCtrl.deleteUser);
