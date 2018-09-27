90ff76608633fdff77a789faf9e4e68b08f2c555
bmirandan <benjamin.miranda.12@sansano.usm.cl>
2018-09-27 04:45:19 +0000

    fix notificaion

--- nodejsAPI/Controllers/Notification.js
+++ nodejsAPI/Controllers/Notification.js
@@ -113,5 +113,6 @@ function postNotificacionAdmin(req, res){ // NOTIFICACION QUE ENVIA EL GUARDIA
 
 module.exports = {
     getNotification,
-    postNotificacion
+    postNotificacionResident,
+    postNotificacionAdmin
 }
