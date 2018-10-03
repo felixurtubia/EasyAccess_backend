2179aa433be2bac83fb3ffb80b58f05f7875d0ee
Felix Urtubia Carrasco <felix.urtubia.13@sansano.usm.cl>
2018-10-02 21:57:03 -0300

    conexión a postgresql
    conexión a servidor de postgresql en scaleway

--- recognitionAPI/recognitionAPI/settings.py
+++ recognitionAPI/recognitionAPI/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'recognitionAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'EasyAccess',
+        'USER':'postgres',
+        'PASSWORD':'boldwareeasyaccess',
+        'HOST':'51.15.240.129',
+        'PORT':'5432',
     }
 }
 
