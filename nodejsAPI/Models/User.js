0f7e6b179b5b85284a8e80852756120969edd7a5
Unknown <allan.viveros.14@sansano.usm.cl>
2018-10-06 19:07:52 -0300

    add relation between User and Department model

--- nodejsAPI/Models/User.js
+++ nodejsAPI/Models/User.js
@@ -5,6 +5,7 @@ const schema = mongoose.Schema;
 
 const userSchema = schema({
     _id: mongoose.Schema.Types.ObjectId,
+    department : { required: true, type: schema.ObjectId, ref: "Department" },
     name: { type: String, required: true },
     lastname: { type: String, required: true },
     rut: { type: String, required: true, trim: true, match: /\d{8}-\d{1}/ },
