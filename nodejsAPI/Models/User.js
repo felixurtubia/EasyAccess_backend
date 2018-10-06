89c232a4725f299bd321d5f9f3eec0a1f9117e21
Unknown <allan.viveros.14@sansano.usm.cl>
2018-10-06 18:36:08 -0300

    add field birthDate in userSchema

--- nodejsAPI/Models/User.js
+++ nodejsAPI/Models/User.js
@@ -4,10 +4,11 @@ const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
 const userSchema = schema({
-    _id : mongoose.Schema.Types.ObjectId,
-    name : { type: String, required: true} ,
-    lastname : { type:  String, required: true},
-    rut : { type:  String, required: true, trim: true, match: /\d{8}-\d{1}/}
+    _id: mongoose.Schema.Types.ObjectId,
+    name: { type: String, required: true },
+    lastname: { type: String, required: true },
+    rut: { type: String, required: true, trim: true, match: /\d{8}-\d{1}/ },
+    birthDate: { type: String, require: true }
 });
 
 module.exports = mongoose.model('User', userSchema);
