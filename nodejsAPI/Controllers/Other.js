1a6a2089808df434529612e92a6c53d71cd9ee88
Unknown <allan.viveros.14@sansano.usm.cl>
2018-10-19 16:44:07 -0300

    add other controller

---
+++ nodejsAPI/Controllers/Other.js
@@ -0,0 +1,25 @@
+'use strict'
+
+//Controllers
+const logCtrl = require('./Log');
+
+/**
+ * Un usuario envia una notificacion al conserje de que un tercero va a entrar
+ * @param {String} idUser id del usuario
+ * @param {String} other informacion sobre el tercero invitado
+ */
+function postOther(req, res) {
+    var idUser = req.body.idUser;
+    var other = req.body.other;
+    console.log("Route: /Other/ [POST] send notification");
+    //LLamar funcion enviar notificacion
+    res.status(200).json({
+        message: "send notification succeed",
+        other : other
+    });
+    logCtrl.logOther(idUser, other);
+}
+
+module.exports = {
+    postOther
+}
