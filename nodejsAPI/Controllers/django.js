219e546123db02a27185fa437a70830f5fa5a7be
Benjamin Miranda <benjamin.miranda.12@sansano.usm.cl>
2018-10-20 04:50:52 -0300

    add create guest on django

--- nodejsAPI/Controllers/django.js
+++ nodejsAPI/Controllers/django.js
@@ -31,6 +31,34 @@ var createUser = function (req, res){
  })
 }
 
+
+var createUserGuest = function (req, res){
+  return new Promise(function(resolve, reject){
+    var options = {
+        method: 'POST',
+
+        uri: 'http://easy.faceapi.boldware.cl/api/guest/',
+
+        form: {
+            idCreador: req.idCreator,
+            idMongo:  req.idUser,
+            image1: req.image1,
+            image2: req.image2,
+            image3: req.image3
+        },
+        headers: {
+            /* 'content-type': 'application/x-www-form-urlencoded' */ // Is set automatically
+        }
+    };
+    rp(options)
+      .then(function (body) {
+        resolve(body);
+      })
+      .catch(function (err) {
+        reject(err);
+      });
+ })
+}
 /**
 * Recibe una imagen correspondiente a la persona con intenciones de ingresar,
 * en este punto por medio de un POST se hace analisis de las imagenes ingresadas
