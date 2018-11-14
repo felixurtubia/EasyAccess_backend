926ff6df734f543c629c8f509393a4a8b30cf21c
allan <allan.viveros.14@sansano.usm.cl>
2018-11-14 13:42:08 -0300

    add standard description guard

--- nodejsAPI/Controllers/Guard.js
+++ nodejsAPI/Controllers/Guard.js
@@ -12,107 +12,117 @@ const Guard = require('../Models/Guard');
  * Entrega todos los guardias 
  */
 function getGuard(req, res) {
-    Guard.find()
-      .exec()
-      .then(docs => {
-        res.status(200).json(docs);
-        console.log("Route: /Guard [GET] Get all Guard");
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({
-          error: err
-        });
+  Guard.find()
+    .exec()
+    .then(docs => {
+      res.status(200).json(docs);
+      console.log("Route: /Guard [GET] Get all Guard");
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
       });
-  }
-  
-  /**
-   * Crear usuario
-   * @param {String} edifice edificio asociado
-   * @param {String} lastname apellido del guardia
-   * @param {String} rut rut del guardia
-   * @param {String} name nombre del guardia
-   * @param {String} phone numeros de celular
-   */
-  function postGuard(req, res) {
-    const guard = Guard({
-      _id: new mongoose.Types.ObjectId(),
-      edifice: "5bca670ccbc43f3ae43cb4ba",
-      name: req.body.name,
-      lastname: req.body.lastname,
-      rut: req.body.rut,
-      phone: "11111111"
     });
-    guard.save()
-      .then(resultado => {
-        console.log("Route: /Guard/ [POST] create Guard now");
-        logCtrl.createLog("Se ha creado un guardia",
-                          "Se ha creado un guardia",
-                          "",
-                          "",
-                          11);
-        res.status(200).json(req.body);
-      }).catch(error => {
-        console.log(error);
-        res.status(500).json({
-          success: false,
-          error: error
-        });
+}
+
+/**
+ * Crear usuario
+ * @param {String} edifice edificio asociado
+ * @param {String} lastname apellido del guardia
+ * @param {String} rut rut del guardia
+ * @param {String} name nombre del guardia
+ * @param {String} phone numeros de celular
+ */
+function postGuard(req, res) {
+  const guard = Guard({
+    _id: new mongoose.Types.ObjectId(),
+    edifice: "5bca670ccbc43f3ae43cb4ba",
+    name: req.body.name,
+    lastname: req.body.lastname,
+    rut: req.body.rut,
+    phone: "11111111"
+  });
+  guard.save()
+    .then(resultado => {
+      console.log("Route: /Guard/ [POST] create Guard now");
+      logCtrl.createLog("Se ha creado un guardia",
+        "Guardia " + resultado.name + " " + resultado.lastname + " se ha creado",
+        "",
+        "",
+        11);
+      res.status(200).json({
+        success: true,
+        guard: resultado
       });
-  }
-  
-  /**
-   * Actualizar algun dato de un usuario mediante su id
-   * @param {String} guardId id del usuario
-   */
-  function updateGuard(req, res) {
-    const guardId = req.params.guardId;
-    Guard.findByIdAndUpdate(guardId , { $set: req.body })
-      .exec()
-      .then(result => {
-        console.log(req.body);
-        logCtrl.createLog("Se ha actualizado los datos del guardia",
-                          "Se ha actualizado los datos del guardia",
-                          "",
-                          "",
-                          12);
-        res.status(200).json(req.body);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({
-          error: err
-        });
+    }).catch(error => {
+      console.log(error);
+      res.status(500).json({
+        success: false,
+        error: error
       });
-  };
-  
-  /**
-   * Eliminar un usuario por su id
-   * @param {String} userId id del usuario (RUTA)
-   */
-  function deleteGuard(req, res) {
-    const guardId = req.params.guardId;
-    Guard.findByIdAndRemove(guardId)
-      .exec()
-      .then(result => {
-        logCtrl.createLog("Se ha eliminado un guardia",
-                      "Se ha eliminado un guardia",
-                      "",
-                      "",
-                      13);
-        res.status(200).json("delete success");
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({
-          error: err
-        });
+    });
+}
+
+/**
+ * Actualizar algun dato de un usuario mediante su id
+ * @param {String} guardId id del usuario
+ */
+function updateGuard(req, res) {
+  const guardId = req.params.guardId;
+  Guard.findByIdAndUpdate(guardId, { $set: req.body }, { new: true })
+    .exec()
+    .then(result => {
+      logCtrl.createLog("Se ha actualizado los datos del guardia",
+        "Se ha actualizado los datos de " + result.name + " " + result.lastname,
+        "",
+        "",
+        12);
+      res.status(200).json({
+        success: true,
+        guard: result
       });
-  }
-  
-  module.exports = {
-    postGuard,
-    getGuard,
-    updateGuard,
-    deleteGuard
-  }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        sucess: false,
+        error: err
+      });
+    });
+};
+
+/**
+ * Eliminar un usuario por su id
+ * @param {String} userId id del usuario (RUTA)
+ */
+function deleteGuard(req, res) {
+  const guardId = req.params.guardId;
+  Guard.findByIdAndRemove(guardId)
+    .exec()
+    .then(result => {
+      logCtrl.createLog("Se ha eliminado un guardia",
+        "Guardia " + result.name + " " + result.lastname + " ha sido eliminado",
+        "",
+        "",
+        13);
+      res.status(200).json({
+        sucess: true,
+        guard: result
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        success: false,
+        error: err
+      });
+    });
+}
+
+module.exports = {
+  postGuard,
+  getGuard,
+  updateGuard,
+  deleteGuard
+}
