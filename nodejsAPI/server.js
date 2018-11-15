20c0a663d30a43c5848b1e03e3a7c3adbadb415b
allan <allan.viveros.14@sansano.usm.cl>
2018-11-15 00:25:31 -0300

    add backup weekend

--- nodejsAPI/server.js
+++ nodejsAPI/server.js
@@ -3,6 +3,7 @@
 const app = require('./app')
 const config = require('./config')
 const database = require('./database');
+const backup = require('./Backup/Backup');
 
 app.listen(config.port);
 console.log(`[ESPERE]: Servidor corriendo en el puerto: ${config.port}`);
