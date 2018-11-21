dab70106fd9621454c2a325b284f12a9b14986d3
Felix Urtubia Carrasco <felix.urtubia.13@sansano.usm.cl>
2018-11-21 15:52:06 -0300

    fix function definition cron
    fix function definitio

--- nodejsAPI/Backup/cron.js
+++ nodejsAPI/Backup/cron.js
@@ -3,4 +3,5 @@ var CronJob = require('cron').CronJob;
 var Cron = require('./mongodb_backup.js');
 new CronJob('* * * * * *', function() {
     Cron.dbAutoBackUp();
+    console.log("Backup DOne ;D")
 }, null, true, 'America/New_York');
