d975e36974aa4a66537dbe93d662286177007288
Benjamin Miranda <benjamin@boldware.cl>
2020-10-31 14:58:58 -0300

    db

--- nodejsAPI/config.js
+++ nodejsAPI/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     port : 80,
     dbCloudMongoose : 'mongodb://localhost:27017/test',
-    dbCloud : "mongodb+srv://Admin:Admin@cluster0-eoomm.mongodb.net/EasyAccess?retryWrites=true",
-    db : "mongodb://Admin:Admin@cluster0-shard-00-00-eoomm.mongodb.net:27017,cluster0-shard-00-01-eoomm.mongodb.net:27017,cluster0-shard-00-02-eoomm.mongodb.net:27017/EasyAccess?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
+    db : "mongodb+srv://Admin:Admin@cluster0-eoomm.mongodb.net/EasyAccess?retryWrites=true",
+    dbCloud : "mongodb://Admin:Admin@cluster0-shard-00-00-eoomm.mongodb.net:27017,cluster0-shard-00-01-eoomm.mongodb.net:27017,cluster0-shard-00-02-eoomm.mongodb.net:27017/EasyAccess?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
 }
